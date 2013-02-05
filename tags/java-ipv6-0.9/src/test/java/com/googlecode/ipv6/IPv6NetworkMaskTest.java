a781fa0ef4cf5db2c139f64717f8858af192e9d3
janvanbesien@gmail.com <janvanbesien@gmail.com@45546c0d-7fb5-4dd7-b8de-d683baba0698>
2013-02-05 09:19:42 +0000

    [maven-release-plugin]  copy for tag java-ipv6-0.9

---
+++ tags/java-ipv6-0.9/src/test/java/com/googlecode/ipv6/IPv6NetworkMaskTest.java
@@ -0,0 +1,42 @@
+package com.googlecode.ipv6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Jan Van Besien
+ */
+public class IPv6NetworkMaskTest
+{
+    @Test
+    public void constructValidNetworkMasks()
+    {
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xffffffffffffffffL, 0xffffffffffffffffL)), new IPv6NetworkMask(128));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xffffffffffffffffL, 0xfffffffffffffffeL)), new IPv6NetworkMask(127));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xffffffffffffffffL, 0xfffffffffffffffcL)), new IPv6NetworkMask(126));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xffffffffffffffffL, 0x8000000000000000L)), new IPv6NetworkMask(65));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xffffffffffffffffL, 0x0L)), new IPv6NetworkMask(64));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0xc000000000000000L, 0x0L)), new IPv6NetworkMask(2));
+        assertEquals(IPv6NetworkMask.fromAddress(new IPv6Address(0x8000000000000000L, 0x0L)), new IPv6NetworkMask(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructInvalidFromPrefixLength_Negative()
+    {
+        new IPv6NetworkMask(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructInvalidFromPrefixLength_TooBig()
+    {
+        new IPv6NetworkMask(129);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructInvalidFromAddress()
+    {
+        IPv6NetworkMask.fromAddress(new IPv6Address(123L, 456L));
+    }
+
+}
